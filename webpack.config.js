caf4f46aa5ecd956c950f7006be2f86f030567a6
oz11l <69080795+oz11l@users.noreply.github.com>
2021-04-22 14:45:53 +0300

    final-commit

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
            "style-loader",
-          "css-loader",
+           "css-loader",
            "sass-loader",
         ],
       },
